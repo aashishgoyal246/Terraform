854f94bc6b39185c78e645e00a35109d1b5c0144
Aashish Goyal <aashishgoyal.246@gmail.com>
2019-08-26 19:01:07 +0530

    Empty

--- terraform-peering-vpc/rds-subnet-groups.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_db_subnet_group" "RDS-SG" {
-  name  = "project-db"
-  description = "This is my rds subnet group for my project"
-  subnet_ids  = ["${aws_subnet.Subnet-2.id}","${aws_subnet.Subnet-3.id}"]
-}
-
